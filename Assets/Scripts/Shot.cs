f1e197d4045f7aa2e73f112e053e3eee2f8b3f18
Darkwizxboy <dlfp16@student.aau.dk>
2018-11-17 00:01:55 +0100

    Created the Shot(). Added bullet prefab.

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shot : MonoBehaviour {
+    //In order for this code to work, drag the bullet prefab into the inspector. 
+    //Create a empty gameobject on the player for the bulletspawn, and drag that into the inspector
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    private GameObject bullet;
+    public float sec = 1f;
+    // Use this for initialization
+    void Start () {
+        gameObject.SetActive(true);
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+            
+        }
+
+
+        
+    }
+    private void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+
+    }
+
+   
+  
 }
