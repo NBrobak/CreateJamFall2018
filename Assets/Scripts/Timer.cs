a415fc5b38aa94032ee497047202c79a8234f922
Unknown <nicolai-brobak@hotmail.com>
2018-11-17 12:48:23 +0100

    Created timer and countdown

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Timer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float gameTime = 120;
+	public float countDownTime = 5;
+
+	[SerializeField]
+	private Text countDownText;
+	[SerializeField]
+	private TextMesh timerText;
+
+	public Action notifyOfEndedGame;
+
+	public float timerContent {
+		get { return float.Parse(timerText.text.Remove(0, 6)); }
+		private set { timerText.text = "Time: " + value.ToString("n2"); }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public float countDownTimerContent
+	{
+		get { return float.Parse(countDownText.text.Trim('.')); }
+		private set { countDownText.text = Mathf.FloorToInt(value) + "..."; }
+	}
+
+	private void Update()
+	{
+		UpdateTimers(Time.timeSinceLevelLoad);
+	}
+
+	private void UpdateTimers(float timePlayed)
+	{
+		timerContent = timePlayed;
+		if (timePlayed > gameTime)
+		{
+			EndGame();
+		}
+		else if (timePlayed > gameTime - countDownTime)
+		{
+			countDownTimerContent = gameTime - timePlayed;
+		}
+	}
+
+	private void EndGame()
+	{
+		if(notifyOfEndedGame != null)
+		{
+			notifyOfEndedGame.Invoke();
+		}
+		gameObject.SetActive(false);
 	}
 }
