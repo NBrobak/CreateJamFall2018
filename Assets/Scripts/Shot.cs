4252c45aadfbdfffb5dd0554f8593b8a7fbe948d
Darkwizxboy <dlfp16@student.aau.dk>
2018-11-17 11:01:46 +0100

    Update Shot.cs

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Shot : MonoBehaviour {
     //In order for this code to work, drag the bullet prefab into the inspector. 
     //Create a empty gameobject on the player for the bulletspawn, and drag that into the inspector
+    public string shootButton;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     private GameObject bullet;
+    private float bulletSpeed = 6.0f;
     public float sec = 1f;
     // Use this for initialization
     void Start () {
@@ -17,7 +19,7 @@ public class Shot : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown(shootButton))
         {
             Fire();
             
@@ -35,7 +37,7 @@ public class Shot : MonoBehaviour {
             bulletSpawn.rotation);
 
         // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
