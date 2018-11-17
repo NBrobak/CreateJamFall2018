a790ee0ca46d262bb854a92ead98f121aa9db714
Unknown <nicolai-brobak@hotmail.com>
2018-11-17 01:52:17 +0100

    Created a spawner to spawn atheist babies

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public bool spawnOnStart = true;
+	public float timeBetweenSpawns = 1;
+	public List<Transform> spawnPositions;
+
+	[SerializeField]
+	private AtheistBaby atheistBabyPrefab;
+
+	private bool isSpawning;
+
+	private void Start()
+	{
+		if (spawnOnStart)
+		{
+			StartSpawning();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void StartSpawning()
+	{
+		StopAllCoroutines();
+		isSpawning = true;
+		StartCoroutine(SpawningCoroutine());
+	}
+
+	public void StopSpawning()
+	{
+		isSpawning = false;
+	}
+
+	private IEnumerator SpawningCoroutine()
+	{
+		yield return new WaitForSeconds(timeBetweenSpawns);
+		while(isSpawning)
+		{
+			SpawnBaby();
+			yield return new WaitForSeconds(timeBetweenSpawns);
+		}
+	}
+
+	private void SpawnBaby()
+	{
+		Transform spawnPosition = spawnPositions[Random.Range(0, spawnPositions.Count)];
+		SpawnBaby(spawnPosition.position);
+	}
+
+	private void SpawnBaby(Vector3 position)
+	{
+		AtheistBaby spawnedBaby = Instantiate(atheistBabyPrefab);
+		spawnedBaby.transform.position = position;
 	}
 }
