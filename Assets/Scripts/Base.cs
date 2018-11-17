510ecc8555c5d591cb1cabbb30d7b6bb536b60e6
Natalie Mark <natalies2005@hotmail.com>
2018-11-17 14:39:36 +0100

    Change in Base

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -13,10 +13,10 @@ public class Base : MonoBehaviour {
 		public bool isOccupied;
 	}
 
-	private Transform[] stationaryLoc;
-	private StationaryBaby[] stationaryBabiesPos;
+    private Transform[] stationaryLoc;
+    private StationaryBaby[] stationaryBabiesPos;
 
-	private Player masterBaby;
+    private Player masterBaby;
 
 	public int Point{
 		get{
@@ -34,52 +34,54 @@ public class Base : MonoBehaviour {
 		private set { masterBaby = value; }
 	}
 
-	// Use this for initialization
-	void Start () {
-		stationaryLoc = this.GetComponentsInChildren<Transform>();
-		stationaryBabiesPos = new StationaryBaby[stationaryLoc.Length];
+    // Use this for initialization
+    void Start()
+    {
+        stationaryLoc = this.GetComponentsInChildren<Transform>();
+        stationaryBabiesPos = new StationaryBaby[stationaryLoc.Length];
 
-		for (int i = 0; i < stationaryBabiesPos.Length; i++)
-		{
-			stationaryBabiesPos[i].xPos = stationaryLoc[i].position.x;
-			stationaryBabiesPos[i].yPos = stationaryLoc[i].position.z;
-			stationaryBabiesPos[i].isOccupied = false;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+        for (int i = 0; i < stationaryBabiesPos.Length; i++)
+        {
+            stationaryBabiesPos[i].xPos = stationaryLoc[i].position.x;
+            stationaryBabiesPos[i].yPos = stationaryLoc[i].position.z;
+            stationaryBabiesPos[i].isOccupied = false;
+        }
+    }
 
-	public void Respawn(){
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Respawn()
+    {
+        masterBaby.gameObject.transform.position = this.gameObject.transform.position;
+    }
+
+    public void Allocate(List<FollowerBaby> babies)
+    {
+        if (babies != null)
+        {
+            point += babies.Count;
+            for (int i = 0; i < stationaryBabiesPos.Length; i++)
+            {
+                if (!stationaryBabiesPos[i].isOccupied && babies.Count != 0)
+                {
+                    stationaryBabiesPos[i].followerBaby = babies[0];
+                    babies[0].targetPos.x = stationaryBabiesPos[i].xPos;
+                    babies[0].targetPos.z = stationaryBabiesPos[i].yPos;
+                    babies[0].targetPos.y = 0f;
+                    babies.RemoveAt(0);
+                }
+            }
+            babies.Clear();
+        }
+    }
+
+	public void LoosePoint(){
 
 	}
-	public void Allocate(List<FollowerBaby> babies){
-		if(babies != null){
-			point += babies.Count;
-			//int babiesForBase = babies.Count < stationaryBabiesPos.Length ? babies.Count : stationaryBabiesPos.Length;
-				for(int i = 0; i < stationaryBabiesPos.Length; i++){
-					if(!stationaryBabiesPos[i].isOccupied && babies.Count != 0){
-						stationaryBabiesPos[i].followerBaby = babies[0];
-						babies[0].targetPos.x = stationaryBabiesPos[i].xPos;
-						babies[0].targetPos.z = stationaryBabiesPos[i].yPos;
-						babies[0].targetPos.y = 0f;
-						babies.RemoveAt(0);
-					}
-				}
-				//plads til babyer
-				//så hvis er baby i pos, gå til næste, osv.
-				//første element fra babies i statbaepos.
-				//remove elementet trukket hen til pos.
-				//tilføj point for alle
-			for(int i = 0; i < stationaryLoc.Length; i++)
-			{
-				
-				//har baby i sig?
-				//har plads, læg deri og giv target pos
-				//giv point
-				//eller kun giv point
-			}
-		}
+	public void GainPoint(){
+		
 	}
 }
