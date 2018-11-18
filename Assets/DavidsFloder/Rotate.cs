7538f1b145a32082fa55131ced7d10c468c6058a
Unknown <dav7id@hotmail.com>
2018-11-18 10:38:45 +0100

    Teddy

---
+++ Assets/DavidsFloder/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float speed = 5;
+    public GameObject bone;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bone.transform.Rotate(Vector3.up * (speed * Time.deltaTime), Space.Self);
+	}
+}
