906b56dd62b2bc83ae04a836292da782466098e9
Nicklas Olsen <nolse16@student.aau.dk>
2018-11-18 01:58:28 +0100

    PointIndicator

---
+++ Assets/Scripts/PointIndicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointIndicator : MonoBehaviour {
+	public Base baseRef;
+	
+	private TextMesh scoreText;
+	// Use this for initialization
+	void Start () {
+		scoreText = transform.GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = baseRef.Point.ToString();
+	}
+}
